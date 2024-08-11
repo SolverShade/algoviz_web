aadbdee28805f82cd428a1ef43738fc3d7936aec
solvershade <seconduacc07@gmail.com>
2024-08-11 09:16:54 -0400

    added unique globe and terminal arrow to sidenav

--- algowiz_client/app/ui/dashboard/nav-links.tsx
+++ algowiz_client/app/ui/dashboard/nav-links.tsx
@@ -3,6 +3,9 @@ import {
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import Image from 'next/image';
+
+const arrowIconPath = '/terminal_arrow_gray.svg';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -27,7 +30,7 @@ export default function NavLinks() {
             href={link.href}
             className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
           >
-            <LinkIcon className="w-6" />
+            <Image src={arrowIconPath} alt="Arrow icon" width={12} height={12} />
             <p className="hidden md:block">{link.name}</p>
           </a>
         );
