2250078a9c69008d089691dffb1bef255f6fa9e6
solvershade <seconduacc07@gmail.com>
2024-08-11 10:06:17 -0400

    further style changes to sidenav

--- algowiz_client/app/ui/acme-logo.tsx
+++ algowiz_client/app/ui/acme-logo.tsx
@@ -1,11 +1,12 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import './acme-logo.css';
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className="logoContainer"
     >
       <Image src="/Globe.svg" alt="Globe icon" width={48} height={48} />
       <p className="text-[36px]">Algowiz</p>
--- algowiz_client/app/ui/dashboard/sidenav.tsx
+++ algowiz_client/app/ui/dashboard/sidenav.tsx
@@ -2,15 +2,15 @@ import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
+import './sidenav.css';
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+    <div className="sidenavContainer">
+      <Link className="logo"
         href="/"
       >
-        <div className="w-32 text-white md:w-40">
+        <div >
           <AcmeLogo />
         </div>
       </Link>
