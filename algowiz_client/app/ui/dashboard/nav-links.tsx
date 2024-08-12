f479972e053367972e74d80935580cf867a5dd3d
solvershade <seconduacc07@gmail.com>
2024-08-12 11:09:25 -0400

    progress on dropdown feature of navlinks sidebar, functional but needs appearance updates

--- algowiz_client/app/ui/dashboard/nav-links.tsx
+++ algowiz_client/app/ui/dashboard/nav-links.tsx
@@ -1,7 +1,75 @@
 'use client'
 import React from 'react';
-import NavLink from './navLink';
+import {useState} from 'react';
 
+export interface NavItem {
+  title: string,
+  parent?: NavItem,
+  subLinks?: NavItem[]
+  href?: string,
+}
+
+export interface NavDropdownProps {
+items: NavItem[];
+}
+
+const NavLinks: React.FC<NavDropdownProps> = ({items}) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <button onClick={toggleDropdown}>
+        {isOpen ? 'Hide' : 'Show'}
+      </button>
+      {isOpen && (
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>
+              {item.title}
+              {item.subLinks && <NavLinks items={item.subLinks} />}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default NavLinks;
+
+/*
+export default function Counter() {
+  const [name, setName] = useState('Taylor');
+  const [age, setAge] = useState(42);
+
+  function handleNameChange(e: { target: { value: React.SetStateAction<string>; }; }) {
+    setName(e.target.value);
+  }
+
+  function handleAgeChange() {
+    setAge(age + 1);
+  }
+
+  return (
+    <>
+      <input
+        value={name}
+        onChange={handleNameChange}
+      />
+      <button onClick={handleAgeChange}>
+        Increment age
+      </button>
+      <p>Hello, {name}. You are {age}.</p>
+    </>
+  )
+}
+*/
+
+/*
 const links: NavLink[] = [
   new NavLink({}, 'sorting algorithms', '',
     [
@@ -11,6 +79,14 @@ const links: NavLink[] = [
     ],
   ),
 ];
+
+interface NavLinkProps {
+  title: string;
+}
+
+function mNavLink({ title }: NavLinkProps){
+  return (<h1>Hello, {title}!</h1>);
+}
 
 export default function NavLinks() {
   return (
@@ -23,3 +99,37 @@ export default function NavLinks() {
     </>
   );
 }
+*/
+
+
+/*
+const links: NavLink[] = [
+  new NavLink({}, 'sorting algorithms', '',
+    [
+      new NavLink({}, 'merge sort', '/merge_sort', []),
+      new NavLink({}, 'quick sort', '/quick_sort', []),
+      new NavLink({}, 'stalin sort', '/stalin_sort', []),
+    ],
+  ),
+];
+
+interface NavLinkProps {
+  title: string;
+}
+
+function mNavLink({ title }: NavLinkProps){
+  return (<h1>Hello, {title}!</h1>);
+}
+
+export default function NavLinks() {
+  return (
+    <>
+      {links.map((link, index) => (
+        <React.Fragment key={index}>
+          {link.render()}
+        </React.Fragment>
+      ))}
+    </>
+  );
+}
+*/
