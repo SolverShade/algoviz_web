b64b38e9cae9d4e112c41ae97a8eca8a66e5cf2d
solvershade <seconduacc07@gmail.com>
2024-08-12 20:25:25 -0400

    sidenav items now have margin at each layer of depth.

--- algowiz_client/app/ui/dashboard/nav-links.tsx
+++ algowiz_client/app/ui/dashboard/nav-links.tsx
@@ -1,149 +1,62 @@
 'use client'
 import React from 'react';
-import {useState} from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 
 export interface NavItem {
   title: string,
   parent?: NavItem,
-  subLinks?: NavItem[]
   href?: string,
+  subLinks?: NavItem[]
+  depth?: number,
 }
 
 export interface NavDropdownProps {
-items: NavItem[];
+  items: NavItem[];
+  depth?: number;
 }
 
-const NavLinks: React.FC<NavDropdownProps> = ({items}) => {
+const NavLinks: React.FC<NavDropdownProps> = ({ items, depth = 0 }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
 
-  return (
-    <div>
-      <button onClick={toggleDropdown}>
-        {isOpen
-          ?
-          <div className='flex'>
-          <Image src='/terminal_arrow_gray.svg' alt="img_missing"
-            width={12} height={12} />
-          <p>Hide</p>
-          </div>
-          :
-          <div className='flex'>
-          <Image src='/terminal_arrow_gray.svg' alt="img_missing"
-            width={12} height={12} />
-          <p>Show</p>
-          </div>
-        }
-      </button>
-      {isOpen && (
-        <ul>
-          {items.map((item, index) => (
-            <li key={index}>
-              {item.title}
-              {item.subLinks && <NavLinks items={item.subLinks} />}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
+  const marginStyle = { marginLeft: `${depth * 20}px` }; // Calculate margin based on depth
+
+  return items.map((item) => {
+    return (
+      <div className='flex flex-col' style={marginStyle} >
+        <button onClick={toggleDropdown}>
+          {isOpen
+            ?
+            <div className='flex'>
+              <Image src='/terminal_arrow_gray.svg' alt="img_missing"
+                width={12} height={12} />
+              <p>{item.title}</p>
+            </div>
+            :
+            <div className='flex'>
+              <Image src='/terminal_arrow_gray.svg' alt="img_missing"
+                width={12} height={12} />
+              <p>{item.title}</p>
+            </div>
+          }
+        </button>
+        {isOpen && (
+          <ul>
+            {items.map((item, index) => (
+              <li key={index}>
+                {item.subLinks &&
+                  <NavLinks items={item.subLinks} depth={depth + 1} />}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    );
+  });
 };
 
 export default NavLinks;
-
-/*
-export default function Counter() {
-  const [name, setName] = useState('Taylor');
-  const [age, setAge] = useState(42);
-
-  function handleNameChange(e: { target: { value: React.SetStateAction<string>; }; }) {
-    setName(e.target.value);
-  }
-
-  function handleAgeChange() {
-    setAge(age + 1);
-  }
-
-  return (
-    <>
-      <input
-        value={name}
-        onChange={handleNameChange}
-      />
-      <button onClick={handleAgeChange}>
-        Increment age
-      </button>
-      <p>Hello, {name}. You are {age}.</p>
-    </>
-  )
-}
-*/
-
-/*
-const links: NavLink[] = [
-  new NavLink({}, 'sorting algorithms', '',
-    [
-      new NavLink({}, 'merge sort', '/merge_sort', []),
-      new NavLink({}, 'quick sort', '/quick_sort', []),
-      new NavLink({}, 'stalin sort', '/stalin_sort', []),
-    ],
-  ),
-];
-
-interface NavLinkProps {
-  title: string;
-}
-
-function mNavLink({ title }: NavLinkProps){
-  return (<h1>Hello, {title}!</h1>);
-}
-
-export default function NavLinks() {
-  return (
-    <>
-      {links.map((link, index) => (
-        <React.Fragment key={index}>
-          {link.render()}
-        </React.Fragment>
-      ))}
-    </>
-  );
-}
-*/
-
-
-/*
-const links: NavLink[] = [
-  new NavLink({}, 'sorting algorithms', '',
-    [
-      new NavLink({}, 'merge sort', '/merge_sort', []),
-      new NavLink({}, 'quick sort', '/quick_sort', []),
-      new NavLink({}, 'stalin sort', '/stalin_sort', []),
-    ],
-  ),
-];
-
-interface NavLinkProps {
-  title: string;
-}
-
-function mNavLink({ title }: NavLinkProps){
-  return (<h1>Hello, {title}!</h1>);
-}
-
-export default function NavLinks() {
-  return (
-    <>
-      {links.map((link, index) => (
-        <React.Fragment key={index}>
-          {link.render()}
-        </React.Fragment>
-      ))}
-    </>
-  );
-}
-*/
