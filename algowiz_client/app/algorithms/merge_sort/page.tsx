9ff4c9ed7b374b95c7c9ec7e3bbf40d52e732d29
solvershade <seconduacc07@gmail.com>
2024-08-14 11:44:33 -0400

    basic circle drawn in visuals section of mergesort, eventually more complex visuals will be created using p5

--- algowiz_client/app/algorithms/merge_sort/page.tsx
+++ algowiz_client/app/algorithms/merge_sort/page.tsx
@@ -1,15 +1,35 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import Link from 'next/link';
-import * as p5 from 'p5';
+'use client'
+import React, { useEffect } from 'react';
+import p5 from 'p5';
 
 export default function Page() {
+  function Circle() {
+    useEffect(() => {
+      new p5((p: p5) => {
+        p.setup = () => {
+          let canvas = p.createCanvas(100, 100);
+          canvas.parent('canvas1');
+          p.background(200);
+          p.circle(50, 50, 25);
+        };
+      },);
+    }, []);
+
+    return <div id='p5Container'></div>;
+  }
+
   return (
-    <div className="content">
-      <h1>welcome to merge sort page. a series of words to test how long
-        the page will take before it splits text into multiple lines. so
-        far there is much text that the page can take and hopefully a line
-        break will occur soon</h1>
+    <div>
+      <div id="content" className="content">
+        <h1>welcome to merge sort page. a series of words to test how long
+          the page will take before it splits text into multiple lines. so
+          far there is much text that the page can take and hopefully a line
+          break will occur soon</h1>
+      </div>
+      <div id='canvas1' className="visuals">
+        <h1>visuals</h1>
+        <Circle></Circle>
+      </div>
     </div>
   );
 }
-
